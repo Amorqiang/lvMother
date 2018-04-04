d100a66a1b1c7bdb8bfb3f952198e65ce5b21940
Amorqiang <292842943@qq.com>
2018-04-04 14:53:17 +0800

    test

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+window.onload = function (ev) {
+    var mySwiper = new Swiper ('.swiper-container', {
+        direction: 'vertical',
+        loop: true,
+        // 如果需要分页器
+
+        autoplay:true
+
+    });
+};
+
